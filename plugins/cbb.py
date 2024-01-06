297d2fda7fac373ab249d004b41da64239966177
Skhan021 <138653770+Skhan021@users.noreply.github.com>
2024-01-06 13:51:42 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Skhan021/File-Sharing-Bot'>Click here</a>\n○ Channel : @Hindi_Dubbed_Anime_Request_Group\n○ Support Group : @SHD_MoviesSearchGroup",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Skhan021/File-Sharing-Bot'>Click here</a>\n○ Channel : @Anime_Hindi_Series\n○ Support Group : @SHD_MoviesSearchGroup",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
